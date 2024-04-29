044bcdacb61489e1f94fcb4f14ac1038c3bc5cf5
apsourg <apostolis@scio.systems>
2024-04-29 17:35:42 +0300

    Remove prints from main

--- main.py
+++ main.py
@@ -32,24 +32,24 @@ def consume_messages():
                 # Initial message consumption may take up to
                 # `session.timeout.ms` for the consumer group to
                 # rebalance and start consuming
-                print("Waiting...")
+                #print("Waiting...")
                 continue
             elif msg.error():
                 if msg.error().code() == KafkaError._PARTITION_EOF:
-                    print("Error partition")
+                    #print("Error partition")
                     continue
                 else:
-                    print(f"Error while consuming message: {msg.error()}")
+                    #print(f"Error while consuming message: {msg.error()}")
                     break
                 print("ERROR: %s".format(msg.error()))
             else:
 
                 # Extract the (optional) key and value, and print.
                 if msg.value() == None:
-                    print("Consumed event to topic {topic}: key = {key:12} value = {value:12}".format(
+                    #print("Consumed event to topic {topic}: key = {key:12} value = {value:12}".format(
                         topic=msg.topic(), key=msg.key().decode('utf-8'), value=""))
                 else:
-                    print("Consumed event to topic {topic}: key = {key:12} value = {value:12}".format(
+                    #print("Consumed event to topic {topic}: key = {key:12} value = {value:12}".format(
                         topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
 
                     input_msg = json.loads(msg.value())
@@ -66,14 +66,14 @@ def consume_messages():
 
                     def delivery_callback(err, msg):
                         if err:
-                            print('ERROR: Message failed delivery: {}'.format(err))
-                            print("Failed to deliver message: %s" % (str(msg)))
+                            #print('ERROR: Message failed delivery: {}'.format(err))
+                            #print("Failed to deliver message: %s" % (str(msg)))
                         else:
                             if msg.value() == None:
-                                print("Produced event to topic {topic}: key = {key:12} value = {value:12}".format(
+                                #print("Produced event to topic {topic}: key = {key:12} value = {value:12}".format(
                                     topic=msg.topic(), key=msg.key().decode('utf-8'), value=""))
                             else:
-                                print("Produced event to topic {topic}: key = {key:12} value = {value:12}".format(
+                                #print("Produced event to topic {topic}: key = {key:12} value = {value:12}".format(
                                     topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
 
 
@@ -84,7 +84,7 @@ def consume_messages():
                     producer.flush()
 
     except Exception as e:
-        print(e)
+        #print(e)
     finally:
         # Leave group and commit final offsets
         consumer.close()
